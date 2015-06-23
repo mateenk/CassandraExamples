a8873082ad29a04edea10b0a961a5da311f0bf74
kstrata <mateenk@kstrata.com>
2015-06-23 12:32:49 +0530

    Adding the project Structure

---
+++ CassandraTemplates/src/main/java/com/kstrata/CassandraTemplates/App.java
@@ -0,0 +1,13 @@
+package com.kstrata.CassandraTemplates;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
